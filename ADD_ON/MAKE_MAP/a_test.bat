0021814c8e9e538341d64f20c880f2ce6f97e55f
Pacman Graphics <gerardwensink@gmail.com>
2020-12-24 13:49:59 +0100

    Tools

---
+++ ADD_ON/MAKE_MAP/a_test.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+set "outputdir_result=map_result" >NUL 2>NUL
+
+"C:\Program Files\IrfanView\i_view64.exe" /panorama=(2,%outputdir_result%\row_HIRES.*.jpg) /convert=%outputdir_result%\clipboard_HIRES.pdf
+"C:\Program Files\IrfanView\i_view64.exe" /panorama=(2,%outputdir_result%\row_small.*.jpg) /convert=%outputdir_result%\clipboard_small.pdf
+"C:\Program Files\IrfanView\i_view64.exe" /panorama=(2,%outputdir_result%\row.*.jpg) /convert=%outputdir_result%\clipboard.pdf
